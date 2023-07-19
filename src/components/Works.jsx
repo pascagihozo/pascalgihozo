9cf0cc002f78ed9cdd0ede73f5856d269d998c5a
pascagihozo <pascagihozo@gmail.com>
2023-07-19 21:43:27 +0400

    experience section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+// import Tilt from 'react-tilt';
+// import {motion} from 'framer-motion';
+
+// import {styles} from '../styles'
+// import{github} from '../assets'
+// import { SectionWrapper } from '../hoc';
+// import {fadeIn, textVariant} from '../utils/motion';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>
+        Projects I worked On
+      </p>
+      <h2 className={styles.sectionHeadText}>
+        Work Experience
+      </h2>
+    </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "");

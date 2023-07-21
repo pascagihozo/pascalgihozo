61f2a66803d09eecefd745d326f1649e1eb7ebc7
pascagihozo <pascagihozo@gmail.com>
2023-07-21 12:46:24 +0400

    content updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,11 +17,50 @@ const Hero = () => {
         <p className={`${styles.heroSubText} mt-2 text-white-100`}>A dedicated software developer,<br className="sm:block hidden"/> with a passion for creating cutting-edge solutions. With a strong background in Computer Science and hands-on experience in Django, Python, React.js, and databases, 
         I'm eager to collaborate on impactful projects. Let's build something extraordinary together!
         </p>
+
+        <div className="flex gap-3 justify-center mt-8">
+        {/* GitHub Button */}
+        <motion.a
+          href="https://github.com/pascagihozo"
+          target="_blank"
+          rel="noopener noreferrer"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="bg-gray-800 hover:bg-gray-900 text-white py-2 px-4 rounded"
+        >
+          GitHub
+        </motion.a>
+
+        {/* LinkedIn Button */}
+        <motion.a
+          href="https://www.linkedin.com/in/pascal-gihozo-a37061178/"
+          target="_blank"
+          rel="noopener noreferrer"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded"
+        >
+          LinkedIn
+        </motion.a>
+
+        {/* Download Resume Button */}
+        <motion.a
+          href="https://docs.google.com/document/d/1ikT5q-A4InsGOD9OCYdvV9Hw_gTpxgcRQ738uPSIvTU/edit?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded"
+        >
+          Download Resume
+        </motion.a>
+      </div>
       
       </div>
       </div>
     
     {/* <ComputersCanvas /> */}
+    
     </section>
   )
 }
